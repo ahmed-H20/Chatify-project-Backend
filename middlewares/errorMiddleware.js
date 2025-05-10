21f41341057c2f43ace79b1dd41ef666abf6ccf3
Fatma Bayomi <your-email@example.com>
2025-05-11 00:12:24 +0300

    chatify

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -42,11 +42,10 @@ import ApiError from "../utils/apiError.js";
 
 const sendErrorForDev = (err, res) => {
     if (res.headersSent) return;
-     res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
+    res.status(err.statusCode || 500).json({
+      status: err.status || 'error',
+      message: err.message,
+      stack: err.stack,
     });
 };
 
@@ -70,11 +69,8 @@ const handleJwtExpired = () =>
     new ApiError('Expired token, please log in again.', 401);
 
 const globalError = (err, req, res, next) => {
-    if (res.headersSent) return next(err);
-    sendErrorForDev(err, req, res);
     err.statusCode = err.statusCode || 500;
     err.status = err.status  || 'error';
-
     if (process.env.NODE_ENV === "development") {
         sendErrorForDev(err, res);
     } else {
